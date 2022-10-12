9fa1a256d46c055d13adc06ea9064a1782a51eff
saroj sundara <sarojkumarsundara518@gmail.com>
2022-10-12 13:47:30 +0530

    integrate all comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import Mission from './components/Mission'
+import "bootstrap/dist/css/bootstrap.css";
+import ThemeBoard from './components/ThemeBoard'
+import ThemeContext from "./context/ThemeContext";
+import { useState } from "react";
+
 
 function App() {
-
+  const [color, setColor] = useState('white');
   return (
     <div className="App">
-      <Mission />
+      {/* <Mission /> */}
+      <ThemeContext.Provider value={{
+        color,
+        setColor
+      }}>
+        <ThemeBoard />
+      </ThemeContext.Provider>
     </div>
   )
 }
